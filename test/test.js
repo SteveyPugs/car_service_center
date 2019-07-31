d43303558fba5d727b482a833ca6da3412473766
Stephen Pugliese <stephen.pugliese@outlook.com>
2019-07-30 23:14:48 -0400

    Adding DB creation to travis

--- test/test.js
+++ test/test.js
@@ -1,12 +1,15 @@
 const assert = require('assert');
+const models = require('../models');
 
-describe('Array', () => {
+describe('Car Service Testing Suite', () => {
 	before(() => {
-		console.log('test');
+		models.createDB();
 	});
-	describe('#indexOf()', () => {
-		it('should return -1 when the value is not present', () => {
-			assert.equal([1, 2, 3].indexOf(4), -1);
+	describe('Users', () => {
+		describe('#indexOf()', () => {
+			it('should return -1 when the value is not present', () => {
+				assert.equal([1, 2, 3].indexOf(4), -1);
+			});
 		});
 	});
 });
