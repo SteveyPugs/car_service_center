38e0cc062ca78fec8b24f9388f7ad21698966167
Stephen Pugliese <stephen.pugliese@outlook.com>
2019-08-01 21:44:14 -0400

    #24
    - Adding in auth to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 import SignIn from './User/SignIn';
 import Forgot from './User/Forgot/Forgot';
@@ -10,6 +10,9 @@ import AppointmentEdit from './Appointment/Edit/Edit';
 import NoMatch from './NoMatch/NoMatch';
 
 class App extends Component {
+    state = {
+        authenticated: false
+    }
     render() {
         return (
             <div className="container">
@@ -18,15 +21,29 @@ class App extends Component {
                         <Route path="/" exact component={SignIn} />
                         <Route path="/forgot" exact component={Forgot} />
                         <Route path="/forgot-step-2" exact component={ForgotConfirmation} />
-                        <Route path="/appointments" exact component={AppointmentSearch} />
-                        <Route path="/appointment/new" exact component={AppointmentNew} />
-                        <Route path="/appointment/edit" exact component={AppointmentEdit} />
+                        <PrivateRoute authenticated={this.state.authenticated} path="/appointments" exact component={AppointmentSearch} />
+                        <PrivateRoute authenticated={this.state.authenticated} path="/appointment/new" exact component={AppointmentNew} />
+                        <PrivateRoute authenticated={this.state.authenticated} path="/appointment/edit" exact component={AppointmentEdit} />
                         <Route component={NoMatch} />
                     </Switch>
                 </BrowserRouter>
             </div>
         );
     }
+}
+
+function PrivateRoute ({component: Component, authenticated, ...rest}) {
+    return (
+        <Route
+            {...rest}
+            render={(props) => authenticated ? <Component {...props} /> : <Redirect to={{
+                pathname: '/',
+                state: {
+                    from: props.location
+                }
+            }} />}
+        />
+    )
 }
 
 export default App;
