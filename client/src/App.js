4f37bc45df320a16bce1b03c11d0dd4b1c5e3202
Stephen Pugliese <stephen.pugliese@outlook.com>
2019-08-02 10:11:52 -0400

    #24
    Added function for adding appointments

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     constructor(props, context) {
         super(props, context);
         this.handleLogin = this.handleLogin.bind(this);
+        this.handleNewAppointment = this.handleNewAppointment.bind(this);
     }
     handleLogin = (event) => {
         event.preventDefault();
@@ -39,6 +40,31 @@ class App extends Component {
             })
         }
     }
+    handleNewAppointment = (event) => {
+        event.preventDefault();
+        if(event.target.checkValidity()){
+            fetch('/appointment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    AppointmentFullName: event.target.AppointmentFullName.value,
+                    AppointmentDate: event.target.AppointmentDate.value,
+                    AppointmentCompleted: event.target.AppointmentCompleted.value,
+                    AppointmentCarMake: event.target.AppointmentCarMake.value,
+                    AppointmentCarModel: event.target.AppointmentCarModel.value,
+                    AppointmentCarYear: event.target.AppointmentCarYear.value,
+                    AppointmentNotes: event.target.AppointmentNotes.value,
+                    ReasonID: event.target.ReasonID.value,
+                })
+            }).then((resp) => resp.json()).then(response => {
+                if(response){
+                    window.location = '/appointments'  
+                }
+            })
+        }
+    }
     render() {
         return (
             <div className="container">
@@ -48,7 +74,7 @@ class App extends Component {
                         <Route path="/forgot" exact component={Forgot} />
                         <Route path="/forgot-step-2" exact component={ForgotConfirmation} />
                         <PrivateRoute path="/appointments" exact component={AppointmentSearch} />
-                        <PrivateRoute path="/appointment/new" exact component={AppointmentNew} />
+                        <PrivateRoute path="/appointment/new" exact component={AppointmentNew} handler={this.handleNewAppointment} />
                         <PrivateRoute path="/appointment/edit" exact component={AppointmentEdit} />
                         <Route component={NoMatch} />
                     </Switch>
@@ -58,11 +84,12 @@ class App extends Component {
     }
 }
 
-function PrivateRoute ({component: Component, ...rest}) {
+function PrivateRoute ({component: Component, handler, ...rest}) {
     return (
         <Route
             {...rest}
-            render={(props) => cookies.get('fe_cookie') ? <Component {...props} /> : <Redirect to={{
+            render={(props) => cookies.get('fe_cookie') ? <Component {...props} handler={handler}
+            /> : <Redirect to={{
                 pathname: '/',
                 state: {
                     from: props.location
--- client/src/Appointment/New/New.js
+++ client/src/Appointment/New/New.js
@@ -1,69 +1,110 @@
-import React from 'react';
-import Navbar from '../../NavBar/Navbar';
+import React, { Component } from 'react';
+import { map, find } from "lodash";
 
-var New = (props) => {
-    return(
-        <div>
-            <br/>
-            <Navbar></Navbar>
-            <br />
-            <div className="container">
-                <h3>New</h3>
-                <hr />
-                <form>
-                    <div className="form-row">
-                        <div className="form-group col-6">
-                            <label>Full Name</label>
-                            <input type="text" className="form-control form-control-sm" id="Name" />
+import Navbar from '../../NavBar/Navbar';
+import carList from '../../car-list';
+
+class New extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modelList: [],
+            reasons: []
+        }
+    }
+    componentDidMount(){
+        fetch('/reasons', {
+            method: 'GET'
+        }).then((resp) => resp.json()).then(response => {
+            this.setState({
+                reasons: response
+            })
+        })
+    }
+    makeChangeHandler = (event) => {
+        let models = find(carList, (car) => {
+            return car.brand === event.target.value;
+        }).models
+        this.setState({
+            modelList: models
+        })
+    }
+    render(){
+        return(
+            <div>
+                <br/>
+                <Navbar></Navbar>
+                <br />
+                <div className="container">
+                    <h3>New</h3>
+                    <hr />
+                    <form onSubmit={this.props.handler} className="needs-validation" noValidate>
+                        <div className="form-row">
+                            <div className="form-group col-6">
+                                <label>Full Name</label>
+                                <input type="text" required className="form-control form-control-sm" name="AppointmentFullName" id="AppointmentFullName" />
+                            </div>
+                            <div className="form-group col-6">
+                                <label>Date / Time</label>
+                                <input type="date" required className="form-control form-control-sm" name="AppointmentDate" id="AppointmentDate" placeholder="Password" />
+                            </div>
                         </div>
-                        <div className="form-group col-6">
-                            <label>Date / Time</label>
-                            <input type="date" className="form-control form-control-sm" id="DateTime" placeholder="Password" />
+                        <div className="form-row">
+                            <div className="form-group col-6">
+                                <label>Reason</label>
+                                <select required name="ReasonID" id="ReasonID" className="form-control form-control-sm" defaultValue="">
+                                    <option value="">Choose...</option>
+                                    {this.state.reasons.map((value, index) => {
+                                        return <option key={index} value={value.ReasonID}>{value.ReasonText + " ($" + value.ReasonPrice + ")"}</option>
+                                    })}
+                                </select>
+                            </div>
+                            <div className="form-group col-6">
+                                <label>Status</label>
+                                <select required name="AppointmentCompleted" id="AppointmentCompleted" className="form-control form-control-sm" defaultValue="">
+                                    <option value="">Choose...</option>
+                                    <option value="0">Open</option>
+                                    <option value="1">Closed</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="form-group col-6">
-                            <label>Reason</label>
-                            <select id="Reason" className="form-control form-control-sm" defaultValue="">
-                                <option>Choose...</option>
-                            </select>
+                        <div className="form-row">
+                            <div className="form-group col-4">
+                                <label>Make</label>
+                                <select required name="AppointmentCarMake" id="AppointmentCarMake" className="form-control form-control-sm" onChange={this.makeChangeHandler}>
+                                    <option value="">Choose...</option>
+                                    {map(carList, 'brand').sort().map((value, index) => {
+                                        return <option key={index} value={value}>{value}</option>
+                                    })}
+                                </select>
+                            </div>
+                            <div className="form-group col-4">
+                                <label>Model</label>
+                                <select required name="AppointmentCarModel" id="AppointmentCarModel" className="form-control form-control-sm">
+                                    <option value="">Choose...</option>
+                                    {this.state.modelList.map((value, index) => {
+                                        return <option key={index} value={value}>{value}</option>
+                                    })}
+                                </select>
+                            </div>
+                            <div className="form-group col-4">
+                                <label>Year</label>
+                                <input type="number" required className="form-control form-control-sm" name="AppointmentCarYear" id="AppointmentCarYear" min="1900" max={(new Date().getFullYear())} defaultValue={(new Date().getFullYear())} />
+                                <div className="invalid-feedback">Enter a year</div>
+                            </div>
                         </div>
-                        <div className="form-group col-6">
-                            <label>Status</label>
-                            <select id="Completed" className="form-control form-control-sm" defaultValue="">
-                                <option>Choose...</option>
-                            </select>
+                        <div className="form-row">
+                            <div className="form-group col">
+                                <label>Notes <small>(optional)</small></label>
+                                <textarea name="AppointmentNotes" id="AppointmentNotes" className="form-control"></textarea>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="form-group col-4">
-                            <label>Make</label>
-                            <select id="Make" className="form-control form-control-sm" defaultValue="">
-                                <option>Choose...</option>
-                            </select>
-                        </div>
-                        <div className="form-group col-4">
-                            <label>Model</label>
-                            <select id="Model" className="form-control form-control-sm" defaultValue="">
-                                <option>Choose...</option>
-                            </select>
-                        </div>
-                        <div className="form-group col-4">
-                            <label>Year</label>
-                            <input type="date" className="form-control form-control-sm" id="Year" min="1900" />
-                        </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="form-group col">
-                            <label>Notes</label>
-                            <textarea className="form-control"></textarea>
-                        </div>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Save</button>
-                </form>
+                        <button type="submit" className="btn btn-primary">Save</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
 
 export default New;
