bf7125e300d157376616416dfec6ca5c30879db0
Stephen Pugliese <stephen.pugliese@outlook.com>
2019-08-02 07:53:27 -0400

    #24

--- client/src/User/SignIn.js
+++ client/src/User/SignIn.js
@@ -12,21 +12,23 @@ var SignIn = (props) => {
                     <div className="card">
                         <div className="card-header text-center">Car Service Login</div>
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={props.handleLogin} className="needs-validation" noValidate>
                                 <div className="form-group">
                                     <label>Email</label>
-                                    <input type="email" className="form-control form-control-sm" id="UserEmail" placeholder="john.doe@example.com" />
+                                    <input type="email" name="UserEmail" id="UserEmail" className="form-control form-control-sm" placeholder="john.doe@example.com" required />
+                                    <div className="invalid-feedback">Enter valid email</div>
                                 </div>
                                 <div className="form-group">
                                     <label>Password</label>
-                                    <input type="password" className="form-control form-control-sm" id="UserPassword" />
+                                    <input type="password" name="UserPassword" id="UserPassword" className="form-control form-control-sm" required />
+                                    <div className="invalid-feedback">Enter password</div>
                                 </div>
-                                <button type="submit" className="btn btn-primary btn-sm btn-block" onClick={props.loginUser}>Sign In</button>
-                                <hr />
-                                <Link to="/forgot" className="btn btn-info btn-sm btn-block">
-                                    Forgot Password
-                                </Link>
+                                <button type="submit" className="btn btn-primary btn-sm btn-block">Sign In</button>
                             </form>
+                            <hr />
+                            <Link to="/forgot" className="btn btn-info btn-sm btn-block">
+                                Forgot Password
+                            </Link>
                         </div>
                     </div>
                 </div>
